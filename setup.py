6545e2639e6ae5728be58ec71f0816aedafa06b9
Ondřej Lanč <ondrej.lanc@gmail.com>
2019-07-02 16:53:59 +0200

    Minor refactoring

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     keywords="ONVIF, Zeep",
     author="Angelcam",
     author_email="dev@angelcam.com",
-    url="https://bitbucket.org/angelcam/onvif-client",
+    url="https://github.com/angelcam/onvif-client",
     license="MIT",
     long_description=open('README.md').read(),
     install_requires=['zeep[async]==2.3.0'],
     packages=['onvif_client'],
-    package_dir={'onvif_client': 'src/onvif_client'},
+    package_dir={'onvif_client': 'onvif_client'},
     package_data={'onvif_client': ['wsdl/*.wsdl', 'wsdl/*.xsd']},
 )
